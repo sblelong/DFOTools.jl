a833bb86a97de0b60d986c35c317cadd37364b3a
sblelong <sblelong+github@zaclys.net>
2025-06-27 17:21:12 -0400

    [algs] LHS functional version

--- src/sample/lhs.jl
+++ src/sample/lhs.jl
@@ -8,21 +8,28 @@ using ResumableFunctions
 
 Perform a latin hypercube sampling with given bounds, and return `p` sampled points. It is assumed that bounds are well-defined.
 """
-@resumable function lhs(n::Int, lb::Vector{Float64}, ub::Vector{Float64}, p::Int; seed::Union{Int,Nothing})::Vector{Vector{Float64}}
+function lhs(n::Int, lb::Vector{Float64}, ub::Vector{Float64}, p::Int; seed::Union{Int,Nothing}=nothing)::Vector{Vector{Float64}}
     # If one of the variables is not bounded, throw error
     if (-INFTY in lb) || (INFTY in ub)
         throw(ArgumentError("Cannot perform LHS: one of the variables is not bounded."))
     end
 
-    if seed
+    if !isnothing(seed)
         Random.seed!(seed)
     end
 
-    Π = [randperm(p) for _ in 1:n]
+    sample = Vector{Float64}[]
+
+    Π = zeros(Int, p, n)
+    for j in 1:n
+        Π[:, j] = randperm(p)
+    end
 
     for i in 1:p
         r = rand(n)
         x = lb .+ ((Π[i, :] .- r) ./ p) .* (ub .- lb)
-        @yield x
+        push!(sample, x)
     end
+
+    return sample
 end
